7eb2b12602b85e63fa265f4c597573ac47844308
Walid Saifi <saifiwalid21@gmail.com>
2024-02-23 10:15:27 +0100

    Update AdminStatsController.php

--- NewFeed/app/Http/Controllers/AdminStatsController.php
+++ NewFeed/app/Http/Controllers/AdminStatsController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminStatsController extends Controller
 {
 
+public function getTotalUsers()
+{
+    $totalUsers = User::count();
+    return response()->json(['total_users' => $totalUsers]);
 }
+
+    public function getUserRegistrationTrend()
+    {
+        $userRegistrations = User::selectRaw('DATE(created_at) as date, COUNT(*) as count')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get();
+
+        $labels = $userRegistrations->pluck('date')->toArray();
+        $data = $userRegistrations->pluck('count')->toArray();
+
+        return response()->json([
+            'labels' => $labels,
+            'data' => $data
+        ]);
+    }
+
+
+}
+
