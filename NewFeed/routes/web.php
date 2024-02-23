63b80961b1a46be59f43c808d0ce200a5ab8f31f
Walid Saifi <saifiwalid21@gmail.com>
2024-02-23 16:35:15 +0100

    mise ajour depuit le main .

---
+++ NewFeed/routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/login', function () {
+    return view('authentication.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('authentication.register');
+})->name('register');
+
+Route::get('/forget', function () {
+    return view('authentication.forget');
+})->name('forget');
+
+Route::get('/reset/{token}',function (){
+    return view('authentication.reset');
+});
+
+Route::get('/',function(){
+    return view('client.home');
+});
+
+Route::get('/dashboard',function(){
+    return view('admin.dashboard');
+})->name('dashboard');
+Route::post('/register/send', [AuthController::class, 'register'])->name('register.send');
+Route::post('/login/send', [AuthController::class, 'login'])->name('login.send');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('get-user', [AuthController::class, 'userInfo']);
+});
