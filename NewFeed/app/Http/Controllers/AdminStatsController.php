660c264bb9e81ef1b653fc9df3bbc8aa7f559ee2
Walid Saifi <saifiwalid21@gmail.com>
2024-02-23 16:21:28 +0100

    Update AdminStatsController.php

--- NewFeed/app/Http/Controllers/AdminStatsController.php
+++ NewFeed/app/Http/Controllers/AdminStatsController.php
@@ -7,37 +7,4 @@ use Illuminate\Http\Request;
 
 class AdminStatsController extends Controller
 {
-<<<<<<< Updated upstream
-
-public function getTotalUsers()
-{
-=======
-    public function getTotalUsers(){
->>>>>>> Stashed changes
-    $totalUsers = User::count();
-    return response()->json(['total_users' => $totalUsers]);
 }
-
-    public function getUserRegistrationTrend()
-    {
-        $userRegistrations = User::selectRaw('DATE(created_at) as date, COUNT(*) as count')
-            ->groupBy('date')
-            ->orderBy('date')
-            ->get();
-
-        $labels = $userRegistrations->pluck('date')->toArray();
-        $data = $userRegistrations->pluck('count')->toArray();
-
-        return response()->json([
-            'labels' => $labels,
-            'data' => $data
-        ]);
-    }
-<<<<<<< Updated upstream
-
-
-}
-
-=======
-}
->>>>>>> Stashed changes
