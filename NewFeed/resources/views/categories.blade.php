dd6085cb6149eca24ee86b14841159af4f9a015a
fatima ezzahra mnaouer <fatimaezzahra.mnaouer@gmail.com>
2024-02-24 23:33:11 +0100

    add edit category

--- NewFeed/resources/views/categories.blade.php
+++ NewFeed/resources/views/categories.blade.php
@@ -6,7 +6,7 @@
                 <div class="row layout-top-spacing">
                     <div class="col-lg-12">
                         <div class="d-flex flex-row-reverse pe-4">
-                            <a id="btn-add-notes" class="btn btn-secondary" href="javascript:void(0);">Add Note</a>
+                            <a id="btn-add-notes" class="btn btn-secondary" href="javascript:void(0);">Add Category</a>
                         </div>
                     </div>
                 </div>
@@ -30,10 +30,12 @@
                                             </div>
                                             <div class="note-action d-flex">
                                                 <div class="pe-3">
-                                                    <a class="fa-solid fa-pencil fa-lg" href="javascript:void(0);"></a>
+                                                    <a onclick="openEditModal({{ $category->id }}, '{{ $category->name }}', '{{ $category->description }}')" class="fa-solid fa-pencil fa-lg" href="javascript:void(0);"></a>
                                                 </div>
                                                 <div>
-                                                    <a class="fa-solid fa-trash fa-lg" href="javascript:void(0);"></a>
+                                                    <form action="">
+                                                        <a class="fa-solid fa-trash fa-lg" href="javascript:void(0);"></a>
+                                                    </form>
                                                 </div>
                                             </div>
                                         </div>
@@ -48,7 +50,7 @@
                             <div class="modal-dialog modal-dialog-centered" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title add-title" id="notesMailModalTitleeLabel">Add Task</h5>
+                                        <h5 class="modal-title add-title" id="notesMailModalTitleeLabel">Add Category</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
                                             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
                                                 <line x1="18" y1="6" x2="6" y2="18"></line>
@@ -88,7 +90,60 @@
                                             </div>
                                         </div>
                                     </div>
-                                    
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal edit -->
+                        <div class="modal fade" id="notesMailModalEdit" tabindex="-1" role="dialog" aria-labelledby="notesMailModalTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title add-title" id="notesMailModalTitleeLabel">Edit Category</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                                            </svg>
+                                        </button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        <div class="notes-box">
+                                            <div class="notes-content">
+
+                                                <form action="{{ route('update-category') }}" id="notesMailModalTitleEdit" method="post">
+                                                    @csrf
+                                                    @method('put')
+
+                                                    <input name="id" type="hidden" id="categoryId">
+                                                    <div class="row">
+                                                        <div class="col-md-12">
+                                                            <div class="d-flex note-title">
+                                                                <input id="nameInput" name="name" type="text" id="n-title" class="form-control" maxlength="25" placeholder="Title">
+                                                            </div>
+                                                            <span class="validation-text"></span>
+                                                        </div>
+
+                                                        <div class="col-md-12">
+                                                            <div class="d-flex note-description">
+                                                                <textarea id="descInput" name="description" id="n-description" class="form-control" maxlength="60" placeholder="Description" rows="3"></textarea>
+                                                            </div>
+                                                            <span class="validation-text"></span>
+                                                            <span class="d-inline-block mt-1 text-danger">Maximum Limit 60 characters</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button class="float-left btn btn-n-save">Save</button>
+                                                        <a class="btn" data-bs-dismiss="modal" href="javascript:void(0);">Discard</a>
+                                                        <button class="btn btn-primary" type="submit">Edit</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
@@ -98,8 +153,4 @@
         </div>
     </div>
     <!--  END CONTENT AREA  -->
-
-    <script>
-
-    </script>
 </x-layouts.admin-layout>
