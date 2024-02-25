a112f2f01e94ad18b90cc72197995ac099619490
PythonScript1 <hplaptopsimo@gmail.com>
2024-02-25 16:03:27 +0100

    ajoutent la table flux_rss

--- NewFeed/database/migrations/2024_02_22_150321_create_flux_rss_table.php
+++ NewFeed/database/migrations/2024_02_22_150321_create_flux_rss_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('source', function (Blueprint $table) {
+        Schema::create('flux_rss', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('url');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('source');
+        Schema::dropIfExists('flux_rss');
     }
 };
