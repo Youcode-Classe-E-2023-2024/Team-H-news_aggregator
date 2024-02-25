461c09f6e26c4dc5efe36ea80061fb494ad1f161
Walid Saifi <saifiwalid21@gmail.com>
2024-02-25 22:32:24 +0100

    Create UsersTableSeeder.php

---
+++ NewFeed/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str; // Import de la classe Str
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 20; $i++) {
+            DB::table('users')->insert([
+                'name' => 'User' . $i,
+                'email' => 'user' . $i . '@example.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10), // Utilisation de Str::random()
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
