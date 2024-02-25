964957f138b6006ded1bb831aa2e572a3674ac11
PythonScript1 <hplaptopsimo@gmail.com>
2024-02-25 19:39:37 +0100

    finaliser le controller FluxRSSController

--- NewFeed/app/Http/Controllers/FluxRSSController.php
+++ NewFeed/app/Http/Controllers/FluxRSSController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\fluxRSS;
+use Illuminate\Pagination\CursorPaginator;
 
 class FluxRSSController extends Controller
 {
@@ -41,36 +43,45 @@ class FluxRSSController extends Controller
 
     public function showRss(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'category' => 'string|min:3',
+            'sort' => 'int',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         $feedUrlsQuery = fluxRSS::query();
 
+        // Sorting
+        switch ($request->sort) {
+            case 1:
+                // newest
+                $feedUrlsQuery->orderBy('created_at', 'desc');
+                break;
+            default:
+                // oldest
+                $feedUrlsQuery->orderBy('created_at');
+        }
+
+        // Filtering by category
         if ($request->category) {
             $feedUrlsQuery->where('category', strip_tags($request->category));
         }
 
+        // Get all feed URLs
         $feedUrls = $feedUrlsQuery->get();
 
-        switch ($request->sort) {
-            case 1:
-                // newest
-                $feedUrls = $feedUrls->sortByDesc('created_at');
-                break;
-            default:
-                // oldest
-                $feedUrls = $feedUrls->sortBy('created_at');
-        }
-
-
-
         $news = [];
-        $errors = [];
 
         foreach ($feedUrls as $feedUrl) {
-
             $feed = simplexml_load_file($feedUrl->url);
             $image = $feed->channel->image->url;
 
             foreach ($feed->channel->item as $item) {
-
                 if (isset($item->children('media', true)->thumbnail['url'])) {
                     $image = (string)$item->children('media', true)->thumbnail['url'];
                 }
@@ -83,8 +94,18 @@ class FluxRSSController extends Controller
             }
         }
 
-        return view('admin.showSources', compact('news', ));
+        // Paginate the news items
+        $perPage = 10;
+        $currentPage = \Illuminate\Pagination\Paginator::resolveCurrentPage() ?: 1;
+        $currentItems = array_slice($news, ($currentPage - 1) * $perPage, $perPage);
+        $newsPaginated = new \Illuminate\Pagination\LengthAwarePaginator($currentItems, count($news), $perPage, $currentPage);
+        $newsPaginated->setPath(route('rss.index'));
 
-
+            return view('admin.showSources', compact('newsPaginated'));
     }
+
+
+
+
+
 }
