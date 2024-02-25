95496100687566d98e020af57a61ad111db90c50
Walid Saifi <saifiwalid21@gmail.com>
2024-02-25 22:32:28 +0100

    Update StatsadminController.php

--- NewFeed/app/Http/Controllers/StatsadminController.php
+++ NewFeed/app/Http/Controllers/StatsadminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class StatsadminController extends Controller
@@ -19,5 +20,26 @@ class StatsadminController extends Controller
         $nombrePublications = Post::count();
         return $nombrePublications;
     }
+
+
+    public function tendanceEnregistrementUtilisateur()
+    {
+        $dateDebut = now()->subDays(5);
+        $dateFin = now();
+
+        $inscriptions = User::whereBetween('created_at', [$dateDebut, $dateFin])
+            ->orderBy('created_at')
+            ->get()
+            ->groupBy(function($date) {
+                return Carbon::parse($date->created_at)->format('Y-m-d');
+            });
+
+        $donnees = $inscriptions->map(function($item, $key) {
+            return $item->count();
+        });
+
+        return view('dashboard.tendance_enregistrement_utilisateur', compact('donnees'));
+    }
+
 
 }
