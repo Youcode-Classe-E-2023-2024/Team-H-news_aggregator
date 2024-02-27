97c9bc92fd7531879dc10446ab80c011fb2a3f69
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-02-27 17:01:40 +0100

    style

--- NewFeed/resources/views/admin/dsplay_users.blade.php
+++ NewFeed/resources/views/admin/dsplay_users.blade.php
@@ -1,3 +1,11 @@
+
+
+{{-- @foreach($users as $user)
+    @dd($user->name);
+    {{-- {{ $user->name }} --}}
+    <!-- Access other user properties as needed -->
+{{-- @endforeach  --}}
+
 <x-layouts.admin-layout title="Add-Ressource" >
     <div id="content" class="main-content">
         <div class="layout-px-spacing">
@@ -36,11 +44,8 @@
                                             <form action="{{route('softdelete', ['user' => $user->id])}}" method="POST" >
                                                 @csrf
                                                 @method('delete')
-                                                <button type="submit" >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 table-cancel"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17">
-                                                        </line><line x1="14" y1="11" x2="14" y2="17"></line>
-                                                    </svg>
-                                                </button>
+                                                <button type="submit" class="btn"><span><i class="fa-solid fa-trash"></i></span></button>
+
                                             </form>   
                                         </td>
                                     </tr>
