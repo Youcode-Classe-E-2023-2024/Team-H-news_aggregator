547494a0b60214f9863c417d879913dcf809cfe0
Walid Saifi <saifiwalid21@gmail.com>
2024-02-28 14:49:17 +0100

    Update StatsadminController.php

--- NewFeed/app/Http/Controllers/StatsadminController.php
+++ NewFeed/app/Http/Controllers/StatsadminController.php
@@ -97,4 +97,34 @@ class StatsadminController extends Controller
 
 
 
+
+    public function getNombreJours()
+    {
+        $nombreEnregistrements = [];
+        for ($i = 6; $i >= 0; $i--) {
+            $date = Carbon::now()->subDays($i);
+            $count = User::whereDate('created_at', $date)->count();
+            $nombreEnregistrements[] = $count;
+        }
+
+        return response()->json(['statsUser' => $nombreEnregistrements]);
+    }
+
+    public function getNombrePostsJours()
+    {
+        $nombrePosts = [];
+        for ($i = 6; $i >= 0; $i--) {
+            $date = Carbon::now()->subDays($i);
+            $count = Post::whereDate('created_at', $date)->count();
+            $nombrePosts[] = $count;
+        }
+
+        return response()->json(['statsPosts' => $nombrePosts]);
+    }
+
+
+
+
+
+
 }
