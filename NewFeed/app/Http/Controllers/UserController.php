b1b929e04ccee23ca8f128feab5c4e7811eb14bf
Walid Saifi <saifiwalid21@gmail.com>
2024-02-29 22:39:19 +0100

    Update UserController.php

--- NewFeed/app/Http/Controllers/UserController.php
+++ NewFeed/app/Http/Controllers/UserController.php
@@ -16,5 +16,27 @@ class UserController extends Controller
         return view('client.home');
     }
 
+    public function update_user(Request $request)
+    {
+        $user = Auth::user();
+
+
+        $user->update([
+            'username' => $request->input('username'),
+
+        ]);
+
+
+        if ($request->hasFile('profile_image')) {
+            // Supprimez les anciennes images s'il y en a
+            $user->clearMediaCollection('profile_images');
+
+            $user->addMediaFromRequest('profile_image')
+                ->toMediaCollection('profile_images');
+        }
+
+        return redirect()->back()->with('success', 'Profil mis à jour avec succès.');
+    }
+
 
 }
